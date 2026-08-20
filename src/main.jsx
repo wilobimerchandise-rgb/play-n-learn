70de7346e28d5a25a984873f07eb899ee64c77d1
wilobimerchandise-rgb <wilobimerchandise@gmail.com>
2026-08-20 22:49:40 +0100

    Wrap App component in React.StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css' // <-- THIS LINE IS VERY IMPORTANT
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
