5f4dc84a6beb3125dba751a425721bb2df45c8ba
wilobimerchandise-rgb <wilobimerchandise@gmail.com>
2026-08-20 23:26:14 +0100

    Remove Firebase initialization and set exports to null
    Set auth and db exports to null, removing Firebase initialization.

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,2 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCy3Sa-tylCABjSYHcjuAL",
-  authDomain: "play-n-learn-children.firebaseapp.com",
-  projectId: "play-n-learn-children",
-  storageBucket: "play-n-learn-children.appspot.com",
-  messagingSenderId: "1068690248417",
-  appId: "1:1068690248417:web:4b4da5b4"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const auth = null;
+export const db = null;
